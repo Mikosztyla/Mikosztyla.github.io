c065969a4d229f73c8d73018f4552b48e525c82c
Mikosztyla <mikosztyla@gmail.com>
2023-11-06 23:02:08 +0100

    data validation

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-const test = document.getElementById("con");
+let inputMessage = document.getElementById('message');
+let numDisplay = document.getElementById("current");
+inputMessage.addEventListener("keyup", countCharacters);
 
-
-test.addEventListener("mouseover", (event)=>{
-    editSwatch();
-});
-
-function editSwatch(){
-    const elem = test.children.item(1);
-    elem.setAttribute("class", "op");
+function countCharacters(){
+    let num = inputMessage.value.length;
+    numDisplay.innerHTML = num;
 }
 
+let email = document.getElementById("email");
+
+function validate(){
+    let emailRegex = /^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$/;
+    let emailValidation = emailRegex.test(email.value);
+    if (!emailValidation) {
+        alert("Zły mail");
+        return false;
+    }
+    return true;
+}
