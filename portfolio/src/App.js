0ea65253c7cb8b1ca39f03f6de0c039975fb539b
Mikosztyla <mikosztyla@gmail.com>
2025-04-02 11:50:27 +0200

    pipeline test

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testing pipeline push
         </a>
       </header>
     </div>
