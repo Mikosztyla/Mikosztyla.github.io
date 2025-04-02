5a82f6d4466fcf5785fa3e22a1c132aa5fe8d894
Mikosztyla <mikosztyla@gmail.com>
2025-04-02 13:39:09 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Portfolio from "./Portfolio";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Testing pipeline push
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Portfolio />
+      </div>
   );
 }
 
