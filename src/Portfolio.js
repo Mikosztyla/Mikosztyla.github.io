be57179ecac1d57cf15b040aa694783bf7c86630
Mikosztyla <mikosztyla@gmail.com>
2025-04-03 12:16:08 +0200

    Finishing touches

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Portfolio.css';
+import { FaGithub, FaLinkedin, FaFacebook, FaGoogle, FaItchIo } from 'react-icons/fa';
 import Roadmap from './RoadMap';
 
 const Portfolio = ({ openProjectDetail }) => {
@@ -7,6 +8,28 @@ const Portfolio = ({ openProjectDetail }) => {
         <div className="portfolio-container">
             <nav className="nav-bar">
                 <h1 className="nav-title">Mikołaj Gosztyła - portfolio</h1>
+                <div className="nav-icons">
+                    <a href={"https://github.com/Mikosztyla"} target="_blank" rel="noopener noreferrer"
+                       className="nav-icon">
+                        <FaGithub size={30}/>
+                    </a>
+                    <a href={"https://www.linkedin.com/in/mikosztyla"} target="_blank" rel="noopener noreferrer"
+                       className="nav-icon">
+                        <FaLinkedin size={30}/>
+                    </a>
+                    <a href={"https://www.facebook.com/Mikosztyla"} target="_blank" rel="noopener noreferrer"
+                       className="nav-icon">
+                        <FaFacebook size={30}/>
+                    </a>
+                    <a href={"mailto:mikosztyla@gmail.com"} target="_blank" rel="noopener noreferrer"
+                       className="nav-icon">
+                        <FaGoogle size={30}/>
+                    </a>
+                    <a href={"https://bulba1265.itch.io/"} target="_blank" rel="noopener noreferrer"
+                       className="nav-icon">
+                        <FaItchIo size={30}/>
+                    </a>
+                </div>
                 <ul className="nav-links">
                     <li><a href="#roadmap" className="nav-link">Career Roadmap</a></li>
                     <li><a href="#projects" className="nav-link">Projects</a></li>
@@ -16,46 +39,57 @@ const Portfolio = ({ openProjectDetail }) => {
 
             <header className="hero-header">
                 Hello!
-                <p className="hero-description">I'm Mikołaj Gosztyła, a third-year Computer Science student at AGH University of Science and Technology. Explore my journey and discover the key milestones of my career and self-made projects.</p>
+                <p className="hero-description">I'm Mikołaj Gosztyła, a third-year Computer Science student at AGH
+                    University of Science and Technology. Explore my journey and discover the key milestones of my
+                    career and self-made projects.</p>
             </header>
 
-            {/* <section id="about" className="about-section">
-                <h2 className="section-title">About Me</h2>
-                <p className="about-description">Hello</p>
-            </section> */}
-
-            <Roadmap />
+            <Roadmap/>
 
             <section id="projects" className="projects-section">
-                <h2 className="section-title">My Projects</h2>
+                <h2 className="section-title">My Games</h2>
                 <div className="projects-grid">
-                    <div className="project-card" onClick={() => openProjectDetail('czasoport')} style={{ backgroundImage: `url(/images/czasoport.png)` }}>
+                    <div className="project-card" onClick={() => openProjectDetail('czasoport')}>
+                        <img className="project-image" src={"/images/czasoport.png"} alt={"czasoport"}/>
                         <h3 className="project-title">Czasoport</h3>
                         <p className="project-description">2D Future Fantasy Adventure and Puzzle game</p>
                     </div>
-                    <div className="project-card" onClick={() => openProjectDetail('castle')}>
-                        <h3 className="project-title">Castle</h3>
-                        <p className="project-description">Mobile app for kids to encourage active sightseeing</p>
-                    </div>
-                    <div className="project-card" onClick={() => openProjectDetail('thiefGame')} style={{ backgroundImage: `url(/images/TheftGame.png)` }}>
+                    <div className="project-card" onClick={() => openProjectDetail('thiefGame')}>
+                        <img className="project-image" src={"/images/TheftGame.png"} alt={"Thief Game"}/>
                         <h3 className="project-title">Thief Game</h3>
-                        <p className="project-description">Will you manage to steal MonaLisa?</p>
+                        <p className="project-description">Will you manage to steal "Mona Lisa"?</p>
                     </div>
-                    <div className="project-card" onClick={() => openProjectDetail('bridgeAnim')}>
-                        <h3 className="project-title">Bridge animation</h3>
-                        <p className="project-description">Animation of walking bridge</p>
-                    </div>
-                    <div className="project-card" onClick={() => openProjectDetail('cubeSolver')} style={{ backgroundImage: `url(/images/CubeSolver.png)` }}>
-                        <h3 className="project-title">Rubik's cube solver</h3>
-                        <p className="project-description">3D Rubik's cube simulation with solver (LBL method)</p>
-                    </div>
-                    <div className="project-card" onClick={() => openProjectDetail('gears')} style={{ backgroundImage: `url(/images/trybiki.png)` }}>
+                    <div className="project-card" onClick={() => openProjectDetail('gears')}>
+                        <img className="project-image" src={"/images/trybiki.png"} alt={"Trycon"}/>
                         <h3 className="project-title">Trycon</h3>
                         <p className="project-description">2D platformer game vampire-survivor like</p>
                     </div>
-                    <div className="project-card" onClick={() => openProjectDetail('lemurs')} style={{ backgroundImage: `url(/images/lemury.png)` }}>
+                    <div className="project-card" onClick={() => openProjectDetail('lemurs')}>
+                        <img className="project-image" src={"/images/lemury.png"} alt={"Lemuringo"}/>
                         <h3 className="project-title">Lemuringo</h3>
-                        <p className="project-description">A 2D top-down educational, writing game developed during HackYeah 2024</p>
+                        <p className="project-description">A 2D top-down educational, writing game developed during
+                            HackYeah 2024</p>
+                    </div>
+                    <div className="project-card" onClick={() => openProjectDetail('castle')}>
+                        <img className="project-image" src={"/images/zamek.png"} alt={"castle"}/>
+                        <h3 className="project-title">Castle</h3>
+                        <p className="project-description">Mobile app for kids to encourage active sightseeing</p>
+                    </div>
+                </div>
+            </section>
+
+            <section id="projects-other" className="projects-section">
+                <h2 className="section-title">Other Projects</h2>
+                <div className="projects-grid">
+                    <div className="project-card" onClick={() => openProjectDetail('bridgeAnim')}>
+                        <img className="project-image" src={"/images/most.png"} alt={"Bridge animation"}/>
+                        <h3 className="project-title">Bridge animation</h3>
+                        <p className="project-description">Animation of walking bridge</p>
+                    </div>
+                    <div className="project-card" onClick={() => openProjectDetail('cubeSolver')}>
+                        <img className="project-image" src={"/images/CubeSolver.png"} alt={"Rubik's cube solver"}/>
+                        <h3 className="project-title">Rubik's cube solver</h3>
+                        <p className="project-description">3D Rubik's cube simulation with solver (LBL method)</p>
                     </div>
                 </div>
             </section>
@@ -63,6 +97,22 @@ const Portfolio = ({ openProjectDetail }) => {
             <section id="contact" className="contact-section">
                 <h2 className="section-title">Contact</h2>
                 <p className="contact-description">Feel free to reach out via email or social media.</p>
+                <a href={"https://github.com/Mikosztyla"} target="_blank" rel="noopener noreferrer"
+                   className="project-link">
+                    <FaGithub size={30}/>
+                </a>
+                <a href={"www.linkedin.com/in/mikosztyla"} target="_blank" rel="noopener noreferrer"
+                   className="project-link">
+                    <FaLinkedin size={30}/>
+                </a>
+                <a href={"https://www.facebook.com/Mikosztyla"} target="_blank" rel="noopener noreferrer"
+                   className="project-link">
+                    <FaFacebook size={30}/>
+                </a>
+                <a href={"mailto:mikosztyla@gmail.com"} target="_blank" rel="noopener noreferrer"
+                   className="project-link">
+                    <FaGoogle size={30}/>
+                </a>
             </section>
         </div>
     );
--- src/ProjectDetail.js
+++ src/ProjectDetail.js
@@ -11,7 +11,8 @@ const ProjectDetail = ({ projectId, closeProjectDetail }) => {
             github: 'https://github.com/Mikosztyla/czasoport',
             youtube: 'https://youtu.be/YTEHO9jmnjw',
             itch: 'https://bulba1265.itch.io/czasoport',
-            responsibilities: 'In this project i did:\n- all the UI animations (time changing animation, item picking, etc)\n- player movement (with blend tree in Unity)\n- implementation of more then 50% of all the puzzles in the game'
+            responsibilities: 'In this project i did:\n- all the UI animations (time changing animation, item picking, etc)\n- player movement (with blend tree in Unity)\n- implementation of more then 50% of all the puzzles in the game',
+            showcase: null
         },
         castle: {
             title: 'Project "Castle"',
@@ -20,52 +21,58 @@ const ProjectDetail = ({ projectId, closeProjectDetail }) => {
             github: null,
             youtube: null,
             itch: null,
-            responsibilities: 'In this project i did:\n- all the UI main screen animations\n- 3d procedural terrain generation'
+            responsibilities: 'In this project i did:\n- all the UI main screen animations\n- 3d procedural terrain generation',
+            showcase: null
         },
         thiefGame: {
-            title: 'Thief game',
-            description: 'A description for project 3.',
+            title: 'Thief Game',
+            description: 'This is my first finished project. As a thief, your goal is to still "Mona Lisa" but it has a little twist at the end... It\' also an educational game, where you can get to know some paintings in the museum, that you want to rob.',
             images: [],
-            github: 'https://github.com/your-username/project3',
-            youtube: null,
+            github: null,
+            youtube: 'https://youtu.be/bbnjuUQ-Wy4',
             itch: null,
-            responsibilities: 'In this project i did:\n- guards movement\n- background animations\n- player animations'
+            responsibilities: 'In this project i did:\n- guards movement\n- player animations',
+            showcase: 'https://youtu.be/81JqwqenrBw'
         },
         bridgeAnim: {
             title: 'Brige animation',
-            description: 'A time tracking app for personal productivity.',
+            description: 'As for the university project my friend and I created a key frame animation of the bridge in python for the 3D bridge model. We were working in the Maya software.',
             images: [],
             github: null,
             youtube: 'https://youtu.be/rxFoa_v7JbY',
             itch: null,
-            responsibilities: ''
+            responsibilities: 'In this project I did:\n - key frame based walking animation',
+            showcase: 'https://youtu.be/rxFoa_v7JbY'
         },
         cubeSolver: {
             title: 'Rubik\'s cube solver',
             description: '3D representation of Rubik\'s cube is renderend without any external libraries. It uses matrix additions and multiplications to render the cube in perspective and enable rotating it. Additionaly, a solver, that return sequences of moves (written in official notation) to perform in order to solve the cube.',
             images: [],
             github: 'https://github.com/Mikosztyla/CubeSolver',
-            youtube: null,
+            youtube: 'https://youtu.be/C1gxVrBb6Ho',
             itch: null,
-            responsibilities: 'This project was developed only by me.'
+            responsibilities: 'This project was developed only by myself.',
+            showcase: null
         },
         gears: {
-            title: '"Trycon"',
+            title: 'Trycon',
             description: 'This 2D platformer vampire-survivor like game was developed during 24h GameJam. The theme of the event was "Trybiki". As a team of 3 developers and 2 graphic designer we craeted a game, where gears are the essential amunition and also your currency. You have to updrade your base (rotate it 180 degrees), before the enemies get you, in order to win.',
             images: [],
-            github: null,
-            youtube: null,
+            github: 'https://github.com/Mikosztyla/immerse-trybiki',
+            youtube: 'https://youtu.be/-Kd6_FO9Kuw',
             itch: null,
-            responsibilities: 'In this project i did:\n'
+            responsibilities: 'In this project i did:\n - modular shop\n - player blend tree animations\n - background animations',
+            showcase: 'https://youtu.be/FYZvwRLlRNU'
         },
         lemurs: {
             title: 'Lemuringo',
-            description: 'A time tracking app for personal productivity.',
+            description: 'A 2D top-down educational game that combines writing and language learning. By utilizing a free, open-source dictionary API, we created an interactive platform where users can learn new words in their chosen language. The game allows players to create their own flashcards and test their knowledge in a fun and engaging way.',
             images: [],
             github: 'https://github.com/Mikosztyla/Lemuringo',
             youtube: 'https://youtu.be/_RzD-e05kFg',
             itch: null,
-            responsibilities: ''
+            responsibilities: 'In this project I did:\n- writing animation\n- boss attacks and animation\n- level selection\n- UI animations',
+            showcase: null
         },
     };
 
@@ -106,6 +113,13 @@ const ProjectDetail = ({ projectId, closeProjectDetail }) => {
                 <h2 className="project-title">{project.title}</h2>
                 <p className="project-description">{project.description}</p>
                 <p className="project-responsibilities">{project.responsibilities}</p>
+                {project.showcase && (
+                    <div className="youtube-link align-left">
+                        <a href={project.showcase} target="_blank" rel="noopener noreferrer" className="project-link">
+                            <FaYoutube size={30} />
+                        </a>
+                    </div>
+                )}
                 <div className="project-images">
                     {project.images.map((img, index) => (
                         <img key={index} src={img} alt={`${project.title} ${index + 1}`} className="project-image" />
